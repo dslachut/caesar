3846d52c79cd5ea5b9326d62244e14ab684ba6bd
David Lachut <dslachut@gmail.com>
2023-10-15 00:30:57 -0500

    Put the map on the home page

--- caesar/pages/index.py
+++ caesar/pages/index.py
@@ -1,17 +1,18 @@
 import dash_bootstrap_components as dbc
 from dash import dcc, html, register_page
 
+from caesar.components import map_div
 from caesar.layout import page_header
 
 page_info = {"path": "/", "title": "CAESAR"}
 
 register_page(__name__, path=page_info["path"], title=page_info["title"], name=page_info["title"])
 
-ph = """## Placeholder
-
-This is placeholder content.
-"""
 
 layout = dbc.Container(
-    [page_header(page_info["title"]), dbc.Container(dcc.Markdown(ph)), html.Hr()],
+    [
+        # page_header(page_info["title"]),
+        map_div,
+        # html.Hr(),
+    ],
 )
